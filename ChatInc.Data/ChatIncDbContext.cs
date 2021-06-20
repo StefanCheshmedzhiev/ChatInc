8e8e44ee736a8709d0659d1208e017b5fdc0bdf7
StefanCheshmedzhiev <s.cheshmedzhiev@abv.bg>
2021-06-20 16:03:36 +0300

    Add DbContext

---
+++ ChatInc.Data/ChatIncDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ChatInc.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatInc.Data
+{
+    public class ChatIncDbContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<User> Users{ get; set; }
+
+        public ChatIncDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
