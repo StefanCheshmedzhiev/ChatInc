3f32c4d4c3a2459b385fd7fda83b65701d41c840
StefanCheshmedzhiev <s.cheshmedzhiev@abv.bg>
2021-06-20 16:04:33 +0300

    Add Models

---
+++ ChatInc.Domain/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChatInc.Domain
+{
+    public class Message
+    {
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
